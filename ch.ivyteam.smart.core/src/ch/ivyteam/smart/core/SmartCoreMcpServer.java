75f65fc9cc3cfcc84ce711816d1e29adbbafdb53
Lukas Giger <lukas.giger@axonivy.com>
2025-09-18 20:40:55 +0200

    --wip-- features

--- ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/SmartCoreMcpServer.java
+++ ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/SmartCoreMcpServer.java
@@ -7,12 +7,16 @@ import javax.servlet.ServletContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.McpServerFeatures.AsyncResourceSpecification;
+import io.modelcontextprotocol.spec.McpSchema.ReadResourceResult;
+import io.modelcontextprotocol.spec.McpSchema.Resource;
 import io.modelcontextprotocol.spec.McpSchema.ServerCapabilities;
+import io.modelcontextprotocol.spec.McpSchema.TextResourceContents;
 
 import ch.ivyteam.ivy.webserver.extension.ServletContextStartupListener;
 import ch.ivyteam.smart.core.mcp.transport.JavaxHttpServletStreamableServerTransportProvider;
-import ch.ivyteam.smart.core.tool.IvyDataClassCreatorTool;
-import ch.ivyteam.smart.core.tool.IvyProcessCreatorTool;
+import ch.ivyteam.smart.core.schema.ResponseSchema;
+import reactor.core.publisher.Mono;
 
 public class SmartCoreMcpServer implements ServletContextStartupListener {
   public static final String SERVLET_PATH = "/smart-core";
@@ -25,10 +29,18 @@ public class SmartCoreMcpServer implements ServletContextStartupListener {
 
     McpServer.async(transportProvider)
         .serverInfo("smart-core-sse", "0.0.1")
-        .capabilities(ServerCapabilities.builder().tools(true).build())
-        .tools(List.of(
-            IvyProcessCreatorTool.specification(),
-            IvyDataClassCreatorTool.specification()))
+        .capabilities(ServerCapabilities.builder().resources(false, false).build())
+        .resources(new AsyncResourceSpecification(
+            Resource.builder()
+                .name("ivy-process-schema")
+                .description("The JSON schema all Axon Ivy processes must adhere to.")
+                .mimeType("application/json")
+                .uri(ResponseSchema.DATA_CLASS.source().toString())
+                .build(),
+            (exchange, request) -> Mono.just(new ReadResourceResult(List.of(new TextResourceContents(
+                ResponseSchema.DATA_CLASS.source().toString(),
+                "application/json",
+                ResponseSchema.PROCESS.schema().toString()))))))
         .build();
 
     var servlet = ctx.addServlet("smart-core-mcp", transportProvider);
