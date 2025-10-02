79cb503231a7a78f7c60be1f0705f16218c5c32c
Lukas Giger <lukas.giger@axonivy.com>
2025-10-02 09:14:04 +0700

    AIAI-190 use schemas checked into core

--- ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/schema/SchemaLoader.java
+++ ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/schema/SchemaLoader.java
@@ -2,7 +2,7 @@ package ch.ivyteam.smart.core.schema;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
+import java.nio.file.Path;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -11,27 +11,15 @@ public class SchemaLoader {
 
   public static final ObjectMapper MAPPER = new ObjectMapper();
 
-  public static ObjectNode readSchema(String resource) {
-    return readSchema(SchemaLoader.class, resource);
+  public static ObjectNode readSchema(Class<?> loader, Path resource) {
+    return read(loader.getResourceAsStream(resource.toString()), resource);
   }
 
-  public static ObjectNode readSchema(Class<?> loader, String resource) {
-    return read(loader.getResourceAsStream(resource), resource);
-  }
-
-  public static ObjectNode readSchema(URI ref) {
-    try (InputStream is = ref.toURL().openStream()) {
-      return read(is, ref.toString());
-    } catch (Exception ex) {
-      throw new RuntimeException("Failed to fetch schema", ex);
-    }
-  }
-
-  public static ObjectNode read(InputStream is, String what) {
+  public static ObjectNode read(InputStream is, Path path) {
     try (is) {
       return (ObjectNode) MAPPER.readTree(is);
     } catch (IOException ex) {
-      throw new RuntimeException("Failed to load schema " + what, ex);
+      throw new RuntimeException("Failed to load schema: " + path.toString(), ex);
     }
   }
 
