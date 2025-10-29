4da3a22165cdf5fc1a08a056584093462486be11
Lukas Giger <lukas.giger@axonivy.com>
2025-10-29 07:39:49 +0100

    AIAI-238 rearrange elements on edit

--- ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/tool/impl/IvyProcessSchemaTool.java
+++ ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/tool/impl/IvyProcessSchemaTool.java
@@ -21,15 +21,17 @@ public class IvyProcessSchemaTool implements IvySchemaTool {
   @Override
   public String guidelines() {
     return """
-      - Process files must be located in a subdirectory of '<project-root>/""" + DIRECTORY_PROCESSES + """
-      /'. Note: This does not apply to form processes.
-      - Omit as many defaults as possible, but at any rate produce the required values.
-      - Create unique instances for element IDs, starting from 'f1'.
-      - Ensure elements are connected to the next one in the sequence flow.
-      - Do not set any visual attributes on elements, except the position 'at'.
-      - Visualize roles as pools.
-      - A process requires a process data class. Create it if needed.
-      - To use the process data class attributes, use 'in.<attribute>' to retrieve data and 'out.<attribute>' to set modified data.""";
+      - Process files must be located in a subdirectory of '<project-root>/""" + DIRECTORY_PROCESSES
+        + """
+          /'. Note: This does not apply to form processes.
+          - Omit as many defaults as possible, but at any rate produce the required values.
+          - Create unique instances for element IDs, starting from 'f1'.
+          - Ensure elements are connected to the next one in the sequence flow.
+          - After any modification, ensure that the elements form a clear, evenly spaced sequence and don't overlap. Adjust existing elements as needed to preserve a visually organized and continuous layout from start to end.
+          - Do not set any visual attributes on elements, except the position 'at'.
+          - Visualize roles as pools.
+          - A process requires a process data class. Create it if needed.
+          - To use the process data class attributes, use 'in.<attribute>' to retrieve data and 'out.<attribute>' to set modified data.""";
   }
 
   @Override
