a6e081b232401e6b93504e5ae5a0714638815357
Lukas Giger <lukas.giger@axonivy.com>
2025-12-02 10:13:13 +0100

    XIVY-17876 introduce integration test bundle
    Tests firing requests against LLM model providers can go here.
    The pipeline is only automatically triggered once a week on master to minimise costs.

---
+++ ch.ivyteam.smart.core.tests.integration/test/ch/ivyteam/smart/core/tool/DataClassSchemaToolSelectionTest.java
@@ -0,0 +1,13 @@
+package ch.ivyteam.smart.core.tool;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class DataClassSchemaToolSelectionTest {
+
+  @Test
+  void happyPipeline() {
+    assertThat(true).isTrue();
+  }
+}
