bb93ad5816dec99e405e18df7e202865422cf904
Alexander Suter <alexander.suter@axonivy.com>
2026-03-11 08:53:24 +0100

    Fix compile error

--- ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/tool/impl/FormSchemaTool.java
+++ ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/tool/impl/FormSchemaTool.java
@@ -1,12 +1,12 @@
 package ch.ivyteam.smart.core.tool.impl;
 
-import static ch.ivyteam.ivy.IvyConstants.DATA_CLASS_EXTENSION;
-import static ch.ivyteam.ivy.IvyConstants.DIRECTORY_SRC_HD;
 import static ch.ivyteam.ivy.IvyConstants.PROCESS_EXTENSION;
 import static ch.ivyteam.ivy.dialog.configuration.FormNameExtensions.EXTENSION;
 import static ch.ivyteam.ivy.dialog.configuration.FormNameExtensions.EXTENSION_WITHOUT_DOT;
+import static ch.ivyteam.ivy.scripting.dataclass.resource.DataClassFileExtensions.DATA_CLASS_EXTENSION;
 import static ch.ivyteam.smart.core.schema.ResourceSchema.FORM;
 
+import ch.ivyteam.ivy.ProjectDir;
 import ch.ivyteam.smart.core.tool.SchemaTool;
 
 public class FormSchemaTool implements SchemaTool {
@@ -24,8 +24,8 @@ public class FormSchemaTool implements SchemaTool {
     (the form process)
     - Use the corresponding tools to acquire the JSON schema and guidelines for the data class and the process before proceeding.
     - The form process must be of kind 'HTML_DIALOG' and contain a 'HtmlDialogStart' connected to a 'HtmlDialogEnd' and a 'HtmlDialogEventStart' connected to a 'HtmlDialogExit'.
-    - The form must be located in a subdirectory of '<project-root>/""" + DIRECTORY_SRC_HD + """
-    /' but not directly in it. At least one other directory between '""" + DIRECTORY_SRC_HD + """
+    - The form must be located in a subdirectory of '<project-root>/""" + ProjectDir.SRC_HD + """
+    /' but not directly in it. At least one other directory between '""" + ProjectDir.SRC_HD + """
     ' and the form directory is required. The directory in which the form is located must have the same name as the form.""";
 
   @Override
