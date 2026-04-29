2ffedc6e4e5fa4497366b65c96407481c77193ec
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-04-29 08:19:04 +0200

    XIVY-18528 adapt stable API for product search

--- ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/tool/market/impl/MarketSearchEngine.java
+++ ch.ivyteam.smart.core/src/ch/ivyteam/smart/core/tool/market/impl/MarketSearchEngine.java
@@ -18,7 +18,8 @@ public class MarketSearchEngine {
 
   private static final Logger LOGGER = Logger.getLogger(MarketSearchEngine.class);
 
-  private static final String MARKET_API = "https://market.axonivy.com/marketplace-service/api";
+  private static final String MARKET_API = "https://market.axonivy.com/stable/api";
+  private static final String MARKET_SERVICE_API = "https://market.axonivy.com/marketplace-service/api";
   private static final Client CLIENT = JerseyClientBuilder.create("market-tool").toClient();
 
   public static String searchProducts(String vendor, String product) {
@@ -34,19 +35,14 @@ public class MarketSearchEngine {
   }
 
   private static WebTarget market() {
-    // https://market.axonivy.com/marketplace-service/api/product?type=all&sort=standard&keyword=&language=en&page=0&size=100&isRESTClient=true
+    // https://market.axonivy.com/stable/api/product?type=all&keyword=&language=en
     return CLIENT.target(MARKET_API).path("/product")
         .queryParam("type", "all")
-        .queryParam("sort", "standard")
-        .queryParam("language", "en")
-        .queryParam("page", 0)
-        .queryParam("size", 200)
-        .queryParam("isRESTClient", true);
+        .queryParam("language", "en");
   }
 
   public static Optional<ArrayNode> enrich(JsonNode json) {
-    var products = Optional.ofNullable(json.get("_embedded"))
-        .map(embedded -> embedded.get("products"))
+    var products = Optional.ofNullable(json)
         .filter(ArrayNode.class::isInstance)
         .map(ArrayNode.class::cast);
     products.get().forEach(MarketSearchEngine::enrichProduct);
@@ -59,7 +55,7 @@ public class MarketSearchEngine {
       if (product.get("_links") instanceof ObjectNode links) {
         if (links.get("self") instanceof ObjectNode self) {
           self.put("publicUri", "https://market.axonivy.com/" + id.asText());
-          self.put("versions", MARKET_API + "/product-details/" + id.asText() + "/versions?designerVersion=&isShowDevVersion=true");
+          self.put("versions", MARKET_API + "/product/" + id.asText() + "/versions?designerVersion=&isShowDevVersion=true");
         }
       }
     }
@@ -67,7 +63,7 @@ public class MarketSearchEngine {
 
   private static JsonNode versions(String id) {
     // https://market.axonivy.com/marketplace-service/api/product-details/coffee-machine-connector/versions?designerVersion=&isShowDevVersion=true
-    var path = CLIENT.target(MARKET_API).path("product-details").path(id).path("versions")
+    var path = CLIENT.target(MARKET_SERVICE_API).path("product-details").path(id).path("versions")
         .queryParam("isShowDevVersion", true);
     var response = path.request().get();
     if (!Family.SUCCESSFUL.equals(response.getStatusInfo().getFamily())) {
